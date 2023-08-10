ef6893f46dbf3c59bd2bb0d1b68cc2fb9ee9da7a
Alex Stroev <stroev.alexander@gmail.com>
2023-08-10 17:30:44 +0300

    Arimo + favicon

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import "./globals.scss";
 
+//
+import { Arimo } from "next/font/google";
+const ff = Arimo({ weight: "400", subsets: ["latin"] });
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ru-RU">
-      <body>{children}</body>
+      <body className={ff.className}>{children}</body>
     </html>
   );
 }
