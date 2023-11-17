07606059be92c9f8309e23951f77947c503c23ab
Alex Stroev <stroev.alexander@gmail.com>
2023-11-17 20:05:46 +0300

    Портфолио вынесено в отдельный компонент

---
+++ app/__components/PortfolioList/index.jsx
@@ -0,0 +1,177 @@
+import PortfolioItem from "../PortfolioItem";
+import styles from "./styles.module.scss";
+
+const PortfolioList = () => {
+    return (
+        <>
+            <h2>Портфолио (выборочно)</h2>
+            <p>
+                Со&nbsp;временем многие сайты исчезают из&nbsp;сети или проходят
+                редизайн (и, соответственно, переверстку). Так что указанное
+                портфолио составляет в&nbsp;лучшем случае несколько процентов
+                от&nbsp;реального.
+            </p>
+            <div className={styles.pfblock}>
+                <div className={styles.pfblock__grid}>
+                    <PortfolioItem
+                        img="/screens/splitconfigurator.png"
+                        title="Конфигуратор коммерческой сплит-системы | Samsung для бизнеса РОССИЯ"
+                        link="https://www.samsung.com/ru/business/system-air-conditioners/single-split-cac/cac-configurator/"
+                        year="2023"
+                    >
+                        <p>
+                            Встроенная область на&nbsp;сайте Samsung
+                            (от&nbsp;верхнего меню до&nbsp;нижних &laquo;хлебных
+                            крошек&raquo;)
+                        </p>
+                        <p>
+                            Никаких шаблонизаторов и&nbsp;библиотек
+                            не&nbsp;использовалось, даже jQuery, чистый&nbsp;JS
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/complex-equipment-program.png"
+                        title="Комплексное оснащение техникой и оборудование шоурумов | Samsung для бизнеса РОССИЯ"
+                        link="https://www.samsung.com/ru/business/complex-equipment-program/"
+                        year="2023"
+                    >
+                        <p>
+                            Встроенная область на сайте Samsung (от верхнего
+                            меню до нижних &laquo;хлебных крошек&raquo;)
+                        </p>
+                        <p>Лендинг</p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/program-for-interior-designers.png"
+                        title="Партнёрская программа для дизайнеров интерьеров и архитекторов | Samsung для бизнеса РОССИЯ"
+                        link="https://www.samsung.com/ru/business/program-for-interior-designers/"
+                        year="2023"
+                    >
+                        <p>
+                            Встроенная область на сайте Samsung (от верхнего
+                            меню до нижних &laquo;хлебных крошек&raquo;)
+                        </p>
+                        <p>Лендинг</p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/program-for-developers.png"
+                        title="Услуги для девелоперов и застройщиков по оснащению объектов строительства | Samsung для бизнеса РОССИЯ"
+                        link="https://www.samsung.com/ru/business/program-for-developers/"
+                        year="2023"
+                    >
+                        <p>
+                            Встроенная область на сайте Samsung (от верхнего
+                            меню до нижних &laquo;хлебных крошек&raquo;)
+                        </p>
+                        <p>Лендинг</p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/samsung-armenia.png"
+                        title="Samsung Армения"
+                        link="https://www.samsung.com/ru/armenia/"
+                        year="2023"
+                    >
+                        <p>
+                            Встроенная область на сайте Samsung (от верхнего
+                            меню до нижних &laquo;хлебных крошек&raquo;)
+                        </p>
+                        <p>Лендинг</p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/tolko-v-dixy.png"
+                        title="Эксклюзивно в Дикси"
+                        link="https://alex-stroev.github.io/dixy_next_gh/"
+                        year="2021"
+                    >
+                        <p>
+                            Первая версия лендинга &laquo;Эксклюзивно
+                            в&nbsp;Дикси&raquo;. Изначально сайт сделан
+                            на&nbsp;чистом React, в&nbsp;2023 году переписан
+                            на&nbsp;Next.js + CSS Modules.
+                        </p>
+                        <p>
+                            Исходники&nbsp;&mdash;{" "}
+                            <a
+                                href="https://github.com/alex-stroev/dixy_next_gh/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                https://github.com/alex-stroev/dixy_next_gh/
+                            </a>
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/calligraphy-museum.png"
+                        title="Музей мировой каллиграфии — лучшие образцы красивого письма в Москве"
+                        link="https://www.calligraphy-museum.com/"
+                        year="2016"
+                    >
+                        <p>Сайт музея мировой каллиграфии, 2016&nbsp;г.</p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/calligraphy-expo.png"
+                        title="Международная выставка каллиграфии"
+                        link="http://calligraphy-expo.com/"
+                        year="2016"
+                    >
+                        <p>
+                            Сайт международной выставки каллиграфии,
+                            2016&nbsp;г.
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/fish-expo.png"
+                        title="Фестиваль &laquo;Ешь рыбу&raquo;, выставка рыбной индустрии. КВЦ Сокольники, 2017&nbsp;г"
+                        link="http://fish-expo.com/"
+                        year="2016"
+                    >
+                        <p>
+                            Лендинг для выставки в&nbsp;КВЦ
+                            &laquo;Сокольники&raquo;, 2016&nbsp;г.
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/foodtruck-fest.png"
+                        title="Фестиваль Фудтраков в Сокольниках"
+                        link="https://foodtruck-fest.ru/"
+                        year="2016"
+                    >
+                        <p>
+                            Лендинг для выставки в&nbsp;КВЦ
+                            &laquo;Сокольники&raquo;, 2016&nbsp;г.
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/bbq-fest.png"
+                        title="Сокольники барбекю-фестиваль"
+                        link="https://bbq-fest.ru/"
+                        year="2016"
+                    >
+                        <p>
+                            Лендинг для выставки в&nbsp;КВЦ
+                            &laquo;Сокольники&raquo;, 2016&nbsp;г.
+                        </p>
+                    </PortfolioItem>
+                    <PortfolioItem
+                        img="/screens/bilet-ru.png"
+                        title="Дешевые авиабилеты онлайн. Поиск билетов на самолет, ЖД билеты, отели. Билет.ру"
+                        link="https://www.bilet.ru/"
+                        year="2004"
+                    >
+                        <p>
+                            Дедушка-бронтозавр из&nbsp;2004&nbsp;года, эпохи
+                            табличной верстки и&nbsp;абсолютного доминирования
+                            IE.
+                        </p>
+                        <p>
+                            Держу в&nbsp;портфолио из&nbsp;сентиментальных
+                            соображений))
+                        </p>
+                    </PortfolioItem>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default PortfolioList;
--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,9 @@ import { Arimo } from "next/font/google";
 const ff = Arimo({ weight: "400", subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ru-RU">
-      <body className={ff.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="ru-RU">
+            <body className={ff.className}>{children}</body>
+        </html>
+    );
 }
