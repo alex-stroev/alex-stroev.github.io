aaa418d94d1309561ed513148125397e3248b4ad
Alex Stroev <stroev.alexander@gmail.com>
2023-11-18 16:07:52 +0300

    + priority

--- app/page.js
+++ app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
                     width={300}
                     height={400}
                     alt={""}
+                    priority={true}
                 />
             </div>
             <p>
