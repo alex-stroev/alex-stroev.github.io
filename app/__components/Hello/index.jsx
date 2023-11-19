233ff7de42f516a431e7efcb6cbe7d3925df248c
Alex Stroev <stroev.alexander@gmail.com>
2023-11-20 01:32:21 +0300

    Hello to CSR

--- app/__components/Hello/index.jsx
+++ app/__components/Hello/index.jsx
@@ -1,24 +1,47 @@
-// "use client";
-const Hello = () => {
-  const time = new Date().getHours();
-  let hello;
+"use client";
+import { useState, useLayoutEffect } from "react";
 
-  switch (true) {
-    case time < 6:
-      hello = "Доброй ночи";
-      break;
-    case time < 12:
-      hello = "Доброе утро";
-      break;
-    case time < 18:
-      hello = "Добрый день";
-      break;
-    case time <= 23:
-      hello = "Добрый вечер";
-      break;
-    default:
-      hello = "Здравствуйте";
-  }
-  return <>{time} {hello}.</>;
+const Hello = () => {
+    const [hello, setHello] = useState("Здравствуйте");
+
+    useLayoutEffect(() => {
+        const hours = new Date().getHours();
+        switch (true) {
+            case hours < 6:
+                setHello("Доброй ночи");
+                break;
+            case hours < 12:
+                setHello("Доброе утро");
+                break;
+            case hours < 18:
+                setHello("Добрый день");
+                break;
+            case hours <= 23:
+                setHello("Добрый вечер");
+                break;
+            default:
+                setHello("Здравствуйте");
+        }
+    }, []);
+
+    //  SSR
+    // let hello;
+    // switch (true) {
+    //     case hours < 6:
+    //         hello = "Доброй ночи";
+    //         break;
+    //     case hours < 12:
+    //         hello = "Доброе утро";
+    //         break;
+    //     case hours < 18:
+    //         hello = "Добрый день";
+    //         break;
+    //     case hours <= 23:
+    //         hello = "Добрый вечер";
+    //         break;
+    //     default:
+    //         hello = "Здравствуйте";
+    // }
+    return <>{hello}.</>;
 };
 export default Hello;
